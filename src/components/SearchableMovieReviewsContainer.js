003335c1897897fb38088173bb8a96383bc1c841
Ola Ouso <ouso.ola@gmail.com>
2020-11-01 12:44:07 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+      super();
+      this.state = {
+        reviews: [],
+        searchTerm: "",
+      };
+    }
+
+    handleSearchInput = (event) => {
+      this.setState({ searchTerm: event.target.value });
+    };
+
+    handleSubmit = (event) => {
+      event.preventDefault();
+      fetch(URL + "&query=" + this.state.searchTerm)
+        .then((resp) => resp.json())
+        .then((data) => this.setState({ reviews: data.results }));
+    };
+
+    render() {
+      return (
+        <div className="searchable-movie-reviews">
+          <form onSubmit={this.handleSubmit}>
+            <label>Search Movie Reviews</label>
+            <input type="text" onChange={this.handleSearchInput}></input>
+            <button type="submit">Submit</button>
+          </form>
+          {this.state.reviews.length > 0}
+          <MovieReviews reviews={this.state.reviews} />
+        </div>
+      );
+    }
+  }
+  export default SearchableMovieReviewsContainer;
