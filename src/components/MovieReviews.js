76b0e462b89756cf75a215ea2f83a914d036226e
Ola Ouso <ouso.ola@gmail.com>
2020-11-01 12:43:49 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = (props) => {
+  const renderReviews = () => {
+    return props.reviews.map((movieReview, index) => (
+      <div className="review" key={movieReview.display_title + index}>
+
+      <h3>{movieReview.display_title}</h3>
+
+      </div>
+    ));
+  };
+
+  return <div className="review-list">{renderReviews()}</div>;
+};
+
+export default MovieReviews;
