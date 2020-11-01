7496c80fb0f7405c92fa664915fbc86bb6c6070a
Ola Ouso <ouso.ola@gmail.com>
2020-11-01 12:44:33 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+      super();
+      this.state = {
+        reviews: [],
+      };
+    }
+
+    componentDidMount() {
+      fetch(URL)
+        .then((response) => response.json())
+        .then((movieReviewData) =>
+          this.setState({ reviews: movieReviewData.results })
+        );
+    }
+
+    render() {
+      return (
+        <div className="latest-movie-reviews">
+          <MovieReviews reviews={this.state.reviews} />
+        </div>
+      );
+    }
+  }
+  export default LatestMovieReviewsContainer;
